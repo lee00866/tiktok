cab143a72afd9a25d55542922c89a44ed856910f
Eunji Lee <lee00866@algonquinlive.com>
2025-07-21 23:52:08 -0400

    Add pushAndRemoveUntil Function

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -23,9 +23,10 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
 
-        Navigator.of(
-          context,
-        ).push(MaterialPageRoute(builder: (context) => InterestsScreen()));
+        Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => InterestsScreen()),
+          (route) => false,
+        );
       }
     }
   }
--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
 
 enum Direction { right, left }
 
@@ -42,6 +43,13 @@ class _TutorialScreenState extends State<TutorialScreen> {
         _showingPage = Page.first;
       });
     }
+  }
+
+  void _onEnterAppTap() {
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(builder: (context) => MainNavigationScreen()),
+      (route) => false,
+    );
   }
 
   @override
@@ -108,7 +116,7 @@ class _TutorialScreenState extends State<TutorialScreen> {
               duration: Duration(milliseconds: 300),
               opacity: _showingPage == Page.first ? 0 : 1,
               child: CupertinoButton(
-                onPressed: () {},
+                onPressed: _onEnterAppTap,
                 color: Theme.of(context).primaryColor,
                 child: Text(
                   'Enter the App!',
