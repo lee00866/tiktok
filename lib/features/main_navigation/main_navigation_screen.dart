b55b0f106b94f7fdd26d6e991a0b9573956b8c9c
Eunji Lee <lee00866@algonquinlive.com>
2025-07-24 14:06:29 -0400

    Add Video Post Screen and video is changed automatically

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/main_navigation/stf_screen.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/post_video_button.dart';
+import 'package:tiktok_clone/features/videos/video_timeline_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   MainNavigationScreen({super.key});
@@ -46,10 +46,10 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          Offstage(offstage: _selectedIndex != 0, child: const StfScreen()),
-          Offstage(offstage: _selectedIndex != 1, child: const StfScreen()),
-          Offstage(offstage: _selectedIndex != 3, child: const StfScreen()),
-          Offstage(offstage: _selectedIndex != 4, child: const StfScreen()),
+          Offstage(offstage: _selectedIndex != 0, child: VideoTimelineScreen()),
+          Offstage(offstage: _selectedIndex != 1, child: Container()),
+          Offstage(offstage: _selectedIndex != 3, child: Container()),
+          Offstage(offstage: _selectedIndex != 4, child: Container()),
         ],
       ),
       bottomNavigationBar: BottomAppBar(
